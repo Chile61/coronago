0991539df794c75741049e59b621b5dad534b44e
Jonas <btxtiger@outlook.com>
2020-03-20 17:55:15 +0100

    Base config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,28 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BootService } from './services/boot.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private bootService: BootService
+    ) {
+        this.initializeApp();
+        this.bootService.initApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
---
+++ src/app/services/boot.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class BootService {
+   constructor() {}
+
+   /**
+    * App services entry point
+    */
+   public initApp(): void {
+      return;
+   }
+}
