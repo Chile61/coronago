b91d17dc628a2c2be49fd8649490dee01a1cf47d
Jonas <btxtiger@outlook.com>
2020-03-25 13:49:52 +0100

    Close app on back

--- src/app/modules/tabs/tabs.page.ts
+++ src/app/modules/tabs/tabs.page.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { APP_ICONS } from '../../ui-components/icons/icons';
+import { Platform } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { ObservableService } from '../../services/observable.service';
 
 @Component({
     selector: 'app-tabs',
     templateUrl: 'tabs.page.html',
-    styleUrls: ['tabs.page.scss']
+    styleUrls: ['tabs.page.scss'],
 })
-export class TabsPage {
+export class TabsPage implements OnInit, OnDestroy {
+    private subscriptions: Subscription[] = [];
+
     public icons = APP_ICONS;
 
-    constructor() {}
+    constructor(private platform: Platform) {}
+
+    ngOnInit(): void {
+        this.subscriptions.push(
+            this.platform.backButton.subscribe(() => {
+                navigator['app'].exitApp();
+            })
+        );
+    }
+
+    ngOnDestroy(): void {
+        ObservableService.unsubscribeFromAll(this.subscriptions);
+    }
 }
