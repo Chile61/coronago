7767773b0b1b7f311aea047b3eba867e7d27f3ce
Jonas <btxtiger@outlook.com>
2020-03-31 18:29:20 +0200

    Language integration

--- src/app/services/i18n.service.ts
+++ src/app/services/i18n.service.ts
@@ -3,6 +3,7 @@ import { registerLocaleData } from '@angular/common';
 import localeGerman from '@angular/common/locales/de';
 import localeEnglish from '@angular/common/locales/en';
 import { TranslateService } from '@ngx-translate/core';
+import { FlagService } from './flag.service';
 
 @Injectable({
     providedIn: 'root',
@@ -10,20 +11,23 @@ import { TranslateService } from '@ngx-translate/core';
 export class I18nService {
     public static activeLocaleKey: string;
 
-    constructor(private translate: TranslateService) {}
+    constructor(private translate: TranslateService, private flagService: FlagService) {}
 
     /**
      * Init service
      */
     public init(): void {
         this.registerLocales();
-        I18nService.activeLocaleKey = 'en-US';
 
         // this language will be used as a fallback when a translation isn't found in the current language
         this.translate.setDefaultLang('en-US');
 
-        // the lang to use, if the lang isn't available, it will use the current loader to get them
-        this.translate.use(I18nService.activeLocaleKey);
+        this.flagService.appLanguage$.subscribe((languageKey) => {
+            I18nService.activeLocaleKey = languageKey;
+
+            // the lang to use, if the lang isn't available, it will use the current loader to get them
+            this.translate.use(I18nService.activeLocaleKey);
+        });
     }
 
     /**
